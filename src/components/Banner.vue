8752e2f4fbb09315ddaada6f58b9b9e789607431
web-Matheus <matheussantosjere02@gmail.com>
2022-05-23 08:07:39 -0300

    falta pequenos ajuste para a finalização do projeto

---
+++ src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-banner">
+        <h1>Make your Burguer</h1>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"Banner"
+}
+</script>
+
+<style scoped>
+    .main-banner{
+       
+    }
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="message-container">
+        <p>{{msg}}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"Message",
+    props:{
+        msg:String
+    }
+}
+</script>
+<style scoped>
+    .message-container{
+        color: #004085;
+        background-color: #cce5ff;
+        border: 2px solid #b8daff;
+        border-radius: 5px;
+        max-width: 400px;
+        padding: 10px;
+        margin: 30px auto;
+    }
+</style>
